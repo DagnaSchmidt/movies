564d59d5f936ff3008db176a20640aae21572337
Dagna Schmidt <dagdagi889@gmail.com>
2024-02-01 14:06:40 +0100

    user profile avatar updated

--- components/navs/topNav/userProfileAvatar.tsx
+++ components/navs/topNav/userProfileAvatar.tsx
@@ -1,9 +1,25 @@
-import { MdOutlinePerson } from "react-icons/md";
+"use client"
+import { useState } from "react";
+import Link from "next/link";
+
+//components
+import Icon from "../../icons/icon";
 
 export default function UserProfileAvatar() {
-    return (
-        <div className="w-11 h-11 flex justify-center items-center">
-            <MdOutlinePerson className="text-white w-5 h-5" />
-        </div>
-    )
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    if (!isLoggedIn) {
+        return (
+            <div className="flex flex-col gap-1 justify-center items-center">
+                <Icon name='User' fill='fill-white' />
+                <p className="text-xs text-white">login</p>
+            </div>
+        )
+    } else {
+        return (
+            <Link href='/profile' className="h-11 w-11 rounded-full bg-primary">
+                {/* user avatar image here */}
+            </Link>
+        )
+    }
 };

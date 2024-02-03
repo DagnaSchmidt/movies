489fc771875a6c0295c275b7b400a0113e86419d
Dagna Schmidt <dagdagi889@gmail.com>
2024-02-03 18:47:38 +0100

    small button added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+import SmallButton from "../components/buttons/smallButton";
 import ArticleCard from "../components/cards/articleCard";
 
 export default function Home() {
@@ -5,6 +7,8 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
             <ArticleCard />
+
+            <SmallButton text="Read more" icon={true} disabled={false} handleClick={() => console.log('cliked')} />
         </main>
     );
 }

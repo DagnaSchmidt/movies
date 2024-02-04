2a7de02eb1024acb6e9338a2e620301d2921b318
Dagna Schmidt <dagdagi889@gmail.com>
2024-02-04 09:59:49 +0100

    article card and small button updated

--- components/cards/articleCard.tsx
+++ components/cards/articleCard.tsx
@@ -1,17 +1,19 @@
+"use client"
 import Image from "next/image";
+import SmallButton from "../buttons/smallButton";
 
 export default function ArticleCard() {
     return (
-        <div className="w-screen h-64 relative">
+        <div className="w-screen max-w-[1024px] h-64 relative">
             <Image
                 alt="article"
                 src="/article.svg"
                 fill
                 className="object-cover"
             />
-            <div className="absolute w-full px-4 py-2 bottom-0 bg-light">
-                <h5>Elizabeth as Princess Diana </h5>
-                {/* button here */}
+            <div className="absolute flex justify-between gap-8 items-center w-full min-h-16 px-4 py-2 bottom-0 bg-linear">
+                <h5 className="text-black font-bold italic text-xl">Elizabeth Debicki as Princess Diana </h5>
+                <SmallButton text="read more" icon={true} disabled={false} handleClick={() => console.log('clicked')} />
             </div>
         </div>
     )

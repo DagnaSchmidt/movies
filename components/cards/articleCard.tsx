4e8988b056e03cc8f3b7f49f9ab36a09f179630b
Dagna Schmidt <dagdagi889@gmail.com>
2024-02-08 08:36:15 +0100

    artile card, article card small, article section updated styling

--- components/cards/articleCard.tsx
+++ components/cards/articleCard.tsx
@@ -14,7 +14,7 @@ export default function ArticleCard() {
                 fill
                 className="object-cover"
             />
-            <div className="absolute flex justify-between gap-8 items-center w-full min-h-16 px-4 py-2 bottom-0 bg-linear">
+            <div className="absolute flex justify-between gap-8 items-center w-full min-h-16 px-4 py-2 bottom-0 bg-light">
                 <h5 className="text-black font-bold italic text-xl">Elizabeth Debicki as Princess Diana </h5>
                 <SmallButton title={true} text="read more" icon={true} disabled={false} handleClick={() => console.log('clicked')} />
             </div>
--- components/sections/home/articlesSection.tsx
+++ components/sections/home/articlesSection.tsx
@@ -6,7 +6,7 @@ export default function ArticlesSection() {
     return (
         <section className="w-full flex flex-col gap-6">
             <ArticleCard />
-            <div className="flex gap-6 w-full px-6 md:px-0">
+            <div className="flex flex-col md:flex-row gap-6 w-full px-6 md:px-0">
                 <ArticleCardSmall image="" title="The fruit of Tim Burton's imagination" alt="text" />
                 <ArticleCardSmall image="" title="The fruit of Tim Burton's imagination" alt="text" />
             </div>

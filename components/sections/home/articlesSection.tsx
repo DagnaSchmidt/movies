1899f71a53884e5343f250b919c5b5aea369021d
Dagna Schmidt <dagdagi889@gmail.com>
2024-02-16 14:35:54 +0100

    articles updated

--- components/sections/home/articlesSection.tsx
+++ components/sections/home/articlesSection.tsx
@@ -7,8 +7,8 @@ export default function ArticlesSection() {
         <section className="w-full flex flex-col gap-6">
             <ArticleCard />
             <div className="flex flex-col md:flex-row gap-6 w-full px-6 md:px-0">
-                <ArticleCardSmall image="" title="The fruit of Tim Burton's imagination" alt="text" />
-                <ArticleCardSmall image="" title="The fruit of Tim Burton's imagination" alt="text" />
+                <ArticleCardSmall id={119051} image="/iHSwvRVsRyxpX7FE7GbviaDvgGZ.jpg" title="The fruit of Tim Burton's imagination" alt="text" />
+                <ArticleCardSmall id={46648} image="/siA2d4PNn4JVFZAwfIYx4pnKCaK.jpg" title={`The new "True Detective" is breaking popularity records`} alt="text" />
             </div>
         </section>
     )
--- data/articlesData.js
+++ data/articlesData.js
@@ -1,7 +1,7 @@
 export const articlesData = [
     {
         id: 65494,
-        main_title: 'Elizabeth Debicki as Princess Diana in season 5 of "The Crown" ',
+        main_title: 'Elizabeth Debicki as Princess Diana in season 5 of "The Crown".',
         paragraph_1: 'Netflix has released a series of photos from the set of the latest season "The Crown". New episodes of the series will hit the platform on November 9. This time the stars of the production will be Imelda Staunton (as Queen Elizabeth) and Elizabeth Debicki, who played the role of Princess Diana.',
         paragraph_2: {
             title: 'What do we know about season 5 of "The Crown"?',
@@ -17,21 +17,35 @@ export const articlesData = [
         ]
     },
     {
-        id: 0,
-        main_title: 'Elizabeth Debicki as Princess Diana in season 5 of "The Crown" ',
-        paragraph_1: "",
+        id: 119051,
+        main_title: "Wednesday is the fruit of Tim Burton's imagination",
+        paragraph_1: "The series 'Wednesday' will have its premiere on the Netflix platform in a month. Tim Burton is responsible for the project. His artistic genius is told in a special video by the stars of the series 'Wednesday' Jenna Ortega, Catherine Zeta-Jones and showrunners Alfred Gough and Miles Millar.",
         paragraph_2: {
-            title: "",
-            text: []
-        }
+            title: "What do we know about the series 'Wednesday'?",
+            text: [
+                'A paranormal detective story set while Wednesday Addams is studying at the Nevermore Academy. Our heroine tries to control her newly discovered psychic powers, stop the murderous forces terrorizing the area and solve the supernatural mystery of which her parents became a part 25 years ago. We will also see how Wednesday finds himself in the maze of relationships with other students at the Nevermore Academy.',
+                "Starring the ghostly clan Jenna Ortega (as Wednesday), Catherine Zeta-Jones (as Morticia Addams), Luis Guzmán (as Gomez Addams), and Isaac Ordonez (as Pugsley Addams). George Burcea will perform as Lurch's faithful butler. They will be accompanied, among others, by Christina Ricci, Gwendoline Christie and Thora Birch."
+            ]
+        },
+        images: [
+            '/xnCDJZR2j4PSfscuwDfCkiCZ4h4.jpg',
+            '/vOz0Pi592OtMevtTGOme1QdcPrz.jpg'
+        ]
     },
     {
-        id: 0,
-        main_title: 'Elizabeth Debicki as Princess Diana in season 5 of "The Crown" ',
-        paragraph_1: "",
+        id: 46648,
+        main_title: 'The new "True Detective" is breaking popularity records',
+        paragraph_1: `The fourth season of the HBO anthology series "True Detective" consists of six episodes. We know the audience data from the first four. And that was enough for the average audience to reach 12.7 million! This broke the previous record, which belonged to the first season of "True Detective" and amounted to $11.9 million. That season was the best-watched new series on HBO since "Six Feet Under."`,
         paragraph_2: {
-            title: "",
-            text: []
-        }
+            title: `About the fourth season of the series "True Detective"`,
+            text: [
+                `As a long winter night falls over Ennis, Alaska, six men working at an Arctic research station disappear without a trace. To solve this case, Liz Danvers and Evangeline Navarro will have to face the darkness inside them and dig out the demons hidden under the eternal ice. The script was written by Issa López ("Tigers Are Not Afraid") and Alan Page Arriaga ("The Shining Girls").`,
+                `Apart from Jodie Foster, the series starred: Kali Reis, John Hawkes, Christopher Eccleston, Fiona Shaw, Finn Bennett and Anna Lambe.`
+            ]
+        },
+        images: [
+            '/vliBilEGlJjuG55n3k5gFWRNHIT.jpg',
+            '/q81lPruIrqjl9iHI06bmnOpNcLX.jpg'
+        ]
     }
 ]

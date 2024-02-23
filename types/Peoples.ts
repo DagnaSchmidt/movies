47e4a8fc3b69221fdc6eb1e4d841b1074142966f
Dagna Schmidt <dagdagi889@gmail.com>
2024-02-23 14:09:47 +0100

    added cast type

--- types/Peoples.ts
+++ types/Peoples.ts
@@ -13,7 +13,22 @@ export type TSinglePersonProps = {
     place_of_birth: string,
     popularity: number,
     profile_path: string
-}
+};
+
+export type TCastProps = {
+    adult: boolean,
+    gender: 1 | 2,
+    id: number,
+    known_for_department: string,
+    name: string,
+    original_name: string,
+    popularity: number,
+    profile_path: string,
+    cast_id: number,
+    character: string,
+    credit_id: string,
+    order: number
+};
 
 export type TCreatorProps = {
     id: number,

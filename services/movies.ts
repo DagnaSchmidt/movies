4557c4510d9784574da6e262e1d9df2b18cfb73b
Dagna Schmidt <dagdagi889@gmail.com>
2024-02-23 14:12:50 +0100

    added service to fetch single movie cast

--- services/movies.ts
+++ services/movies.ts
@@ -10,3 +10,9 @@ export const getSingleMovieData = async (id: number) => {
     const response = await axios.get(url, config);
     return response.data;
 };
+
+export const getSingleMovieCast = async (id: number) => {
+    const url = `https://api.themoviedb.org/3/movie/${id}/credits?language=en-US`;
+    const response = await axios.get(url, config);
+    return response.data;
+};
